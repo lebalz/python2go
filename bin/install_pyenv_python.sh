c0f41ca60b3f9ad0c0fad04b767edcf9e65ae838
Balthasar Hofer <lebalz@outlook.com>
2020-07-05 14:29:25 +0200

    install and configure python for osx

---
+++ bin/install_pyenv_python.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# install.sh
+
+
+if [ -z $1 ]; then
+  PYTHON_VERSION="3.8.3"
+else
+  PYTHON_VERSION=$1
+fi
+
+echo "installing pyenv"
+brew install pyenv
+
+if ! pyenv version | grep $PYTHON_VERSION; then
+  # make sure tcl-tk is installed s.t. it can be configured for python
+  echo "installing tkinter©"
+  brew install tcl-tk
+
+
+  echo "installing python $PYTHON_VERSION"
+  # install python with the current version of tcl-tk
+  env \
+    PATH="$(brew --prefix tcl-tk)/bin:$PATH" \
+    LDFLAGS="-L$(brew --prefix tcl-tk)/lib" \
+    CPPFLAGS="-I$(brew --prefix tcl-tk)/include" \
+    PKG_CONFIG_PATH="$(brew --prefix tcl-tk)/lib/pkgconfig" \
+    CFLAGS="-I$(brew --prefix tcl-tk)/include" \
+    PYTHON_CONFIGURE_OPTS="--with-tcltk-includes='-I$(brew --prefix tcl-tk)/include' --with-tcltk-libs='-L$(brew --prefix tcl-tk)/lib -ltcl8.6 -ltk8.6'" \
+    pyenv install $PYTHON_VERSION
+else
+  echo "python $PYTHON_VERSION already installed"
+fi
+
+pyenv global $PYTHON_VERSION
+
+touch ~/.zshrc
+if ! grep -q 'if command -v pyenv 1>/dev/null 2>&1; then' ~/.zshrc; then
+  echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+fi
+
+touch ~/.bash_profile
+if ! grep -q 'if command -v pyenv 1>/dev/null 2>&1; then' ~/.bash_profile; then
+  echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
+fi
--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,8 @@ import { Progress } from "vscode";
 import { promisify } from "util";
 import { exec, execSync } from "child_process";
 
+const PYTHON_VERSION = "3.8.3";
+
 function promptRootPassword(
   context: vscode.ExtensionContext
 ): Thenable<string | undefined> {
@@ -19,8 +21,10 @@ function isBrewInstalled() {
   return shellExec("brew -v")
     .then(({ stdout, stderr }) => {
       if (stdout.length > 0) {
+        vscode.window.showInformationMessage("Brew already installed...");
         return true;
       }
+      vscode.window.showErrorMessage(stderr);
       return false;
     })
     .catch(() => {
@@ -28,45 +32,97 @@ function isBrewInstalled() {
     });
 }
 
-function installBrew(context: vscode.ExtensionContext, rootPW: string) {
+function installBrew(
+  context: vscode.ExtensionContext
+): Promise<boolean | undefined> {
   return isBrewInstalled()
-    .then(async (isInstalled) => {
+    .then((isInstalled) => {
       if (isInstalled) {
         return true;
       }
-      const shellExec = promisify(exec);
-      return shellExec(
-        `cat -s ${`${context.extensionPath}/bin/install_brew.sh`} | bash -s "${rootPW}" && echo "Success."`
-      )
+      promptRootPassword(context)
+        .then((rootPW) => {
+          if (!rootPW) {
+            return { stdout: "", stderr: "ERROR: no root password" };
+          }
+          const shellExec = promisify(exec);
+          return shellExec(
+            `cat -s ${`${context.extensionPath}/bin/install_brew.sh`} | bash -s "${rootPW}" && echo "Success."`
+          );
+        })
         .then(({ stdout, stderr }) => {
           if (!stdout || !stdout.endsWith("Success.\n")) {
             vscode.window.showErrorMessage(
               `Could not install install brew. Try to install it manually and try the setup process again.\n${stderr}`
             );
-            return;
+            return false;
           }
-
           return true;
-        })
-        .catch(() => {
-          return false;
         });
     })
-    .catch(() => {
+    .catch((error) => {
+      vscode.window.showErrorMessage(error);
       return false;
     });
 }
 
-function installPython(context: vscode.ExtensionContext) {
-  if (process.platform === "darwin") {
-    promptRootPassword(context).then((rootPW) => {
-      if (!rootPW) {
-        return new Promise(() => false);
+function installPythonWithPyEnv(
+  context: vscode.ExtensionContext
+): Promise<boolean> {
+  const shellExec = promisify(exec);
+  return shellExec(
+    `cat -s ${`${context.extensionPath}/bin/install_pyenv_python.sh`} | bash -s "${PYTHON_VERSION}" && echo "Success."`
+  )
+    .then(({ stdout, stderr }) => {
+      if (!stdout || !stdout.endsWith("Success.\n")) {
+        vscode.window.showErrorMessage(
+          `Could not install install python.\n${stderr}`
+        );
+        return false;
       }
-      installBrew(context, rootPW);
+      return true;
+    })
+    .catch((error) => {
+      vscode.window.showErrorMessage(error);
+      return false;
+    });
+}
+
+function installPython(
+  context: vscode.ExtensionContext
+): Promise<boolean | undefined> {
+  if (process.platform === "darwin") {
+    return installBrew(context).then((success) => {
+      if (!success) {
+        return false;
+      }
+      return installPythonWithPyEnv(context);
     });
   } else if (process.platform === "win32") {
+    return new Promise(() => false);
   }
+  return new Promise(() => false);
+}
+
+function configure(context: vscode.ExtensionContext, force: boolean = false) {
+  const configuration = vscode.workspace.getConfiguration();
+  if (process.platform === "darwin") {
+    return configuration
+      .update(
+        "python.pythonPath",
+        `~/.pyenv/versions/${PYTHON_VERSION}/bin/python`,
+        vscode.ConfigurationTarget.Global
+      )
+      .then(() => {
+        configuration.update(
+          "python.defaultInterpreterPath",
+          `~/.pyenv/versions/${PYTHON_VERSION}/bin/python`,
+          vscode.ConfigurationTarget.Global
+        );
+      });
+  } else if (process.platform === "win32") {
+  };
+  return new Promise(() => {});
 }
 
 // this method is called when your extension is activated
@@ -80,10 +136,13 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand("python2go.install", () => {
-    // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from python2go!");
+    installPython(context).then((success) => {
+      if (success) {
+        return configure(context, true);
+      }
+    }).then(() => {
+      vscode.window.showInformationMessage("Python installed and configured. Ready to go");
+    });
   });
 
   context.subscriptions.push(disposable);
